b88f1394138a98837ef0dcd32330940bc721f115
atomsprint <atoms_print@studyatoms.com>
2026-02-13 10:46:05 -0800

    Add files via upload
    Initial commit

---
+++ BookRecommender/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  BookRecommender
+//
+//  Created by Takato Ohyabu on 2/13/26.
+//
+
+import Foundation
+
+struct Book {
+    let age: String
+    let gender: String
+    let title: String
+    let author: String
+}
